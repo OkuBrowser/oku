0d13710f4f2bb2d4add87512ced5a54322810c0f
Oku <MadeByEmil@users.noreply.github.com>
2020-12-02 22:12:12 +0000

    Automatically enforce Rust styleguide

--- src/main.rs
+++ src/main.rs
@@ -14,61 +14,88 @@
     You should have received a copy of the GNU Affero General Public License
     along with Oku.  If not, see <https://www.gnu.org/licenses/>.
 */
-use std::path::Path;
-use std::collections::HashMap;
-use futures::TryStreamExt;
-use ipfs_api::IpfsClient;
-use std::fs;
 use async_recursion::async_recursion;
 use directories::ProjectDirs;
+use futures::TryStreamExt;
+use ipfs_api::IpfsClient;
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
 
 #[actix_rt::main]
 async fn main() {
-    let project_directories = ProjectDirs::from("org", "Emil Sayahi",  "Oku").unwrap();
+    let project_directories = ProjectDirs::from("org", "Emil Sayahi", "Oku").unwrap();
     let client = IpfsClient::default();
     let test = "bafybeicqqzygir3ysia2ivmhdwi6znvuivrl2yqtmapp57fuz4zk6ugweq".to_string();
-    let local_directory = format!("{}/{}", project_directories.cache_dir().to_str().unwrap(), test);
+    let local_directory = format!(
+        "{}/{}",
+        project_directories.cache_dir().to_str().unwrap(),
+        test
+    );
     let mut hierarchy = HashMap::new();
     hierarchy.insert(test.to_owned(), local_directory.to_owned());
-    ipfs_download_directory(&client, local_directory.to_owned(), test.to_owned(), hierarchy).await;
+    ipfs_download_directory(
+        &client,
+        local_directory.to_owned(),
+        test.to_owned(),
+        hierarchy,
+    )
+    .await;
 }
 
-async fn ipfs_download_file(client: &IpfsClient, file_hash: String, file_path: String)
-{
-    match client.cat(&file_hash).map_ok(|chunk| chunk.to_vec()).try_concat().await
+async fn ipfs_download_file(client: &IpfsClient, file_hash: String, file_path: String) {
+    match client
+        .cat(&file_hash)
+        .map_ok(|chunk| chunk.to_vec())
+        .try_concat()
+        .await
     {
         Ok(res) => {
             println!("Writing: {} ({}) … ", file_path, file_hash);
             fs::create_dir_all(Path::new(&file_path[..]).parent().unwrap()).unwrap();
             fs::write(file_path, &res).unwrap();
         }
-        Err(e) => eprintln!("Failed to obtain file: {} ({})\nError: {:#?}", file_path, file_hash, e)    
+        Err(e) => eprintln!(
+            "Failed to obtain file: {} ({})\nError: {:#?}",
+            file_path, file_hash, e
+        ),
     }
 }
 
 #[async_recursion(?Send)]
-async fn ipfs_download_directory(client: &IpfsClient, directory: String, directory_hash: String, mut hierarchy: HashMap<String, String>)
-{
+async fn ipfs_download_directory(
+    client: &IpfsClient,
+    directory: String,
+    directory_hash: String,
+    mut hierarchy: HashMap<String, String>,
+) {
     hierarchy.insert(directory_hash.clone(), directory.clone());
     let directory_object = client.file_ls(&directory_hash).await.unwrap().objects;
-    for object in directory_object
-    {
-        for link in object.1.links
-        {
+    for object in directory_object {
+        for link in object.1.links {
             let link_type = &link.typ.clone().unwrap();
-            match &link_type.as_str()
-            {
+            match &link_type.as_str() {
                 &"Directory" => {
-                    let sub_directory = format!("{}/{}", hierarchy.get(&directory_hash).unwrap(), link.name);
-                    ipfs_download_directory(&client, sub_directory.to_owned(), link.hash.clone(), hierarchy.clone()).await;
+                    let sub_directory =
+                        format!("{}/{}", hierarchy.get(&directory_hash).unwrap(), link.name);
+                    ipfs_download_directory(
+                        &client,
+                        sub_directory.to_owned(),
+                        link.hash.clone(),
+                        hierarchy.clone(),
+                    )
+                    .await;
                     hierarchy.insert(link.hash, sub_directory);
                 }
                 &"File" => {
-                    ipfs_download_file(&client, link.hash, format!("{}/{}", hierarchy.get(&directory_hash).unwrap(), link.name)).await;
+                    ipfs_download_file(
+                        &client,
+                        link.hash,
+                        format!("{}/{}", hierarchy.get(&directory_hash).unwrap(), link.name),
+                    )
+                    .await;
                 }
-                &_ => {
-
-                }
+                &_ => {}
             }
         }
     }
