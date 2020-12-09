28e45be01f304cbfa6233de7b808c5e46e34098d
Emil Sayahi <em@emsa.cf>
2020-12-08 19:46:50 -0500

    Codebase cleaning!

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@
     along with Oku.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+use gtk::Inhibit;
 use async_recursion::async_recursion;
 use directories_next::ProjectDirs;
 
@@ -37,7 +38,6 @@ lazy_static! {
         ProjectDirs::from("org", "Emil Sayahi", "Oku").unwrap();
 }
 
-//#[actix_rt::main]
 fn main() {
     let cache_directory = PROJECT_DIRECTORIES.cache_dir().to_str().unwrap();
     let client = IpfsClient::default();
@@ -49,58 +49,36 @@ fn main() {
     let glade_src = include_str!("window.glade");
     let web_kit = webkit2gtk::WebViewBuilder::new();
     web_kit.build();
-    //let web_kit_functions = webkit2gtk::WebViewExt::
     let builder = gtk::Builder::from_string(glade_src);
 
     let window: gtk::Window = builder.get_object("window").unwrap();
-    let refresh_button: gtk::Button = builder.get_object("refresh_button").unwrap();
-    //let webkit_settings: webkit2gtk_sys::WebKitSettings = builder.get_object("webkit_settings").unwrap();
+    let go_button: gtk::Button = builder.get_object("go_button").unwrap();
     let nav_entry: gtk::Entry = builder.get_object("nav_entry").unwrap();
     let web_view: webkit2gtk::WebView = builder.get_object("webkit_view").unwrap();
     let _web_settings: webkit2gtk::Settings = builder.get_object("webkit_settings").unwrap();
 
-    // refresh_button.connect_clicked(move |_| {
-    //     dialog.run();
-    //     dialog.hide();
-    // });
-
-    refresh_button.connect_clicked(move |_refresh_button| {
+    go_button.connect_clicked(move |_go_button| {
         let hash = nav_entry.get_text().to_string();
         let local_directory = format!("{}/{}", cache_directory, hash);
-        // rt.spawn(future::lazy(|_| {
-        //     get_from_hash(client.clone(), hash, local_directory.clone());
-        //  }));
 
         get_from_hash(client.clone(), hash, local_directory.clone());
-        // let mut rt = Runtime::new().unwrap();
-        // rt.block_on(future);
-        // let runtime = Builder::new_multi_thread()
-        //     .worker_threads(4)
-        //     .thread_name("oku-thread")
-        //     .thread_stack_size(3 * 1024 * 1024)
-        //     .build()
-        //     .unwrap();
 
-        // runtime.block_on(future);
         web_view.load_uri(&format!("file:///{}/index.html", &local_directory));
-        //web_view.load_uri("https://crates.io/");
         println!("Loading: {} … ", web_view.get_uri().unwrap().to_string());
     });
 
     window.show_all();
-
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
     gtk::main();
-
-    //let test = "bafybeidd5ronzlgzm4t2upk32zxzxofmyoxv4sdwsded5kc5i4enf7myoe".to_string();
-    //get_from_hash(&client, test, cache_directory).await;
 }
 
 fn get_from_hash(client: IpfsClient, hash: String, local_directory: String) {
     let mut hierarchy = HashMap::new();
     hierarchy.insert(hash.to_owned(), local_directory.to_owned());
-    //let mut rt = actix_rt::Runtime::new().unwrap();
     let mut sys = actix_rt::System::new("name: T");
-    //let sys_man = actix_rt::System::current();
     sys.block_on(async move {
         ipfs_download_directory(
             &client,
@@ -111,15 +89,6 @@ fn get_from_hash(client: IpfsClient, hash: String, local_directory: String) {
         .await;
         println!("{}", local_directory.clone());
     });
-    //sys.run().unwrap();
-    //sys_man.stop();
-    // ipfs_download_directory(
-    //     &client,
-    //     local_directory.to_owned(),
-    //     hash.to_owned(),
-    //     hierarchy,
-    // )
-    // .await;
 }
 
 async fn ipfs_download_file(client: &IpfsClient, file_hash: String, file_path: String) {
