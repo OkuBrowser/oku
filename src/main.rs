c609c5baa0e7ab43beb57b92cb970f7cf018587d
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2021-01-01 17:45:24 -0500

    Directly accessing gateway - No need for many dependencies - No need to directly download to local cache (letting WebKit handle caching) - Error pages and messages are now handled - Relative URLs are now handled
    What this doesn't improve:
    - UI
    - Built-in IPFS gateway
    - Link support beyond relative links (this can be fixed by catching WebKit's loading event and making a simple redirect)
    - Branding
    - Hyperdrive support
    - macOS and Windows compatibility
    - Documenting codebase
    
    Things to potentially improve:
    - Error pages are ugly
    - Directory listings have links the user probably doesn't want (like the download page to IPFS)

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,15 @@
     along with Oku.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-use async_recursion::async_recursion;
 use directories_next::ProjectDirs;
 use gtk::Inhibit;
 use percent_encoding::percent_decode_str;
 
-use futures::TryStreamExt;
 use glib::clone;
 use gtk::prelude::BuilderExtManual;
 use gtk::ButtonExt;
 use gtk::EntryExt;
 use gtk::WidgetExt;
-use ipfs_api::IpfsClient;
-use std::collections::HashMap;
-use std::fs;
-use std::path::Path;
 use url::{Position, Url};
 use webkit2gtk::WebViewExt;
 
@@ -42,13 +36,11 @@ lazy_static! {
 }
 
 fn main() {
-    let cache_directory = PROJECT_DIRECTORIES.cache_dir().to_str().unwrap();
-    let client = IpfsClient::default();
-
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
         return;
     }
+
     let glade_src = include_str!("window.glade");
     let web_kit = webkit2gtk::WebViewBuilder::new();
     web_kit.build();
@@ -65,19 +57,18 @@ fn main() {
         {
             let hash = &url[Position::BeforeHost..];
             let decoded_hash = percent_decode_str(&hash.to_owned()).decode_utf8().unwrap().to_string();
-            let local_directory = format!("{}/{}", cache_directory, decoded_hash);
-
-            get_from_hash(client.clone(), decoded_hash, local_directory.clone());
-
-            web_view.load_uri(&format!("file://{}", &local_directory));
-
-            println!("Loading: file://{} … \n", &local_directory);
+            let split_hash: Vec<&str> = decoded_hash.split('/').collect();
+            let path = &decoded_hash.replacen(split_hash[0], "", 1).replacen('/', "", 1);
+            let gateway_url = format!("http://{}.ipfs.localhost:8080/{}", split_hash[0], path);
+            web_view.load_uri(&format!("{}", &gateway_url));
+            println!("Loading: {} … ", &gateway_url);
         } else {
             web_view.load_uri(&nav_entry.get_text().to_string());
         }
 
         web_view.connect_load_changed(clone!(@weak web_view, @weak nav_entry => move |_, _| {
-            nav_entry.set_text(&web_view.get_uri().unwrap().to_string().replacen(&format!("file://{}/", cache_directory), "ipfs://", 1));
+            let conformant_url = &web_view.get_uri().unwrap().to_string().replacen("http://", "ipfs://", 1).replacen(".ipfs.localhost:8080", "", 1);
+            nav_entry.set_text(conformant_url);
         }));
     });
 
@@ -87,83 +78,4 @@ fn main() {
         Inhibit(false)
     });
     gtk::main();
-}
-
-fn get_from_hash(client: IpfsClient, hash: String, local_directory: String) {
-    let mut hierarchy = HashMap::new();
-    hierarchy.insert(hash.to_owned(), local_directory.to_owned());
-    let mut sys = actix_rt::System::new("Oku IPFS System");
-    sys.block_on(async move {
-        /*ipfs_download_directory(
-            &client,
-            local_directory.to_owned(),
-            hash.to_owned(),
-            hierarchy,
-        )
-        .await;*/
-        ipfs_download_file(&client, hash.to_owned(), local_directory.to_owned()).await;
-        println!(
-            "Requesting: {} (local: {}) … \n",
-            hash.to_owned(),
-            local_directory.to_owned()
-        );
-    });
-}
-
-async fn ipfs_download_file(client: &IpfsClient, file_hash: String, file_path: String) {
-    match client
-        .cat(&file_hash)
-        .map_ok(|chunk| chunk.to_vec())
-        .try_concat()
-        .await
-    {
-        Ok(res) => {
-            println!("\nWriting: {} ({}) … \n", file_path, file_hash);
-            fs::create_dir_all(Path::new(&file_path[..]).parent().unwrap()).unwrap();
-            fs::write(file_path, &res).unwrap();
-        }
-        Err(e) => eprintln!(
-            "\nFailed to obtain file: {} ({})\nError: {:#?}\n",
-            file_path, file_hash, e
-        ),
-    }
-}
-
-#[async_recursion(?Send)]
-async fn ipfs_download_directory(
-    client: &IpfsClient,
-    directory: String,
-    directory_hash: String,
-    mut hierarchy: HashMap<String, String>,
-) {
-    hierarchy.insert(directory_hash.clone(), directory.clone());
-    let directory_object = client.file_ls(&directory_hash).await.unwrap().objects;
-    for object in directory_object {
-        for link in object.1.links {
-            let link_type = &link.typ.clone().unwrap();
-            match link_type.as_str() {
-                "Directory" => {
-                    let sub_directory =
-                        format!("{}/{}", hierarchy.get(&directory_hash).unwrap(), link.name);
-                    ipfs_download_directory(
-                        &client,
-                        sub_directory.to_owned(),
-                        link.hash.clone(),
-                        hierarchy.clone(),
-                    )
-                    .await;
-                    hierarchy.insert(link.hash, sub_directory);
-                }
-                "File" => {
-                    ipfs_download_file(
-                        &client,
-                        link.hash,
-                        format!("{}/{}", hierarchy.get(&directory_hash).unwrap(), link.name),
-                    )
-                    .await;
-                }
-                _ => {}
-            }
-        }
-    }
 }
