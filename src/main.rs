bdd50cfaee44fd23ffa8e76c71e641169ca0d64d
Emil Sayahi <em@emsa.cf>
2021-01-02 04:45:57 -0500

    Better nav handling Broke IPFS somehow when adding enter key functionality. Investigate later

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,55 @@ lazy_static! {
         ProjectDirs::from("org", "Emil Sayahi", "Oku").unwrap();
 }
 
+fn connect(nav_entry: gtk::Entry, web_view: webkit2gtk::WebView)
+{
+    let mut nav_text = nav_entry.get_text().to_string();
+
+    if !nav_text.contains("://")
+    {
+        nav_text = format!("http://{}", nav_text);
+    }
+
+    let url = Url::parse(&nav_text).unwrap();
+
+    match url.scheme()
+    {
+        "ipfs" => {
+            let hash = &url[Position::BeforeHost..];
+            let decoded_hash = percent_decode_str(&hash.to_owned()).decode_utf8().unwrap().to_string();
+            let split_hash: Vec<&str> = decoded_hash.split('/').collect();
+            let path = &decoded_hash.replacen(split_hash[0], "", 1).replacen('/', "", 1);
+            let gateway_url = format!("http://{}.ipfs.localhost:8080/{}", split_hash[0], path);
+            web_view.load_uri(&gateway_url);
+            println!("Loading: {} … ", &gateway_url);
+        }
+        _ => {
+            web_view.load_uri(&nav_text);
+            println!("Loading: {} … ", &nav_text);
+        }
+    }
+}
+
+fn on_load(nav_entry: gtk::Entry, web_view: webkit2gtk::WebView)
+{
+    let mut nav_text = nav_entry.get_text().to_string();
+
+    if !nav_text.contains("://")
+    {
+        nav_text = format!("http://{}", nav_text);
+    }
+    println!("{}", nav_text);
+
+    let parsed_url = Url::parse(&nav_text).unwrap();
+    let mut url = web_view.get_uri().unwrap().to_string();
+    let cid = parsed_url[Position::BeforeHost..].split('/').collect::<Vec<&str>>()[0];
+    if url.starts_with(&format!("http://{}.ipfs.localhost:8080/", cid))
+    {
+        url = url.replacen("http://", "ipfs://", 1).replacen(".ipfs.localhost:8080", "", 1);
+    }
+    nav_entry.set_text(&url);
+}
+
 fn main() {
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
@@ -51,26 +100,15 @@ fn main() {
     let nav_entry: gtk::Entry = builder.get_object("nav_entry").unwrap();
     let web_view: webkit2gtk::WebView = builder.get_object("webkit_view").unwrap();
 
-    go_button.connect_clicked(move |_go_button| {
-        let url = Url::parse(&nav_entry.get_text().to_string()).unwrap();
-        if url.scheme() == "ipfs"
-        {
-            let hash = &url[Position::BeforeHost..];
-            let decoded_hash = percent_decode_str(&hash.to_owned()).decode_utf8().unwrap().to_string();
-            let split_hash: Vec<&str> = decoded_hash.split('/').collect();
-            let path = &decoded_hash.replacen(split_hash[0], "", 1).replacen('/', "", 1);
-            let gateway_url = format!("http://{}.ipfs.localhost:8080/{}", split_hash[0], path);
-            web_view.load_uri(&&gateway_url);
-            println!("Loading: {} … ", &gateway_url);
-        } else {
-            web_view.load_uri(&nav_entry.get_text().to_string());
-        }
-
-        web_view.connect_load_changed(clone!(@weak web_view, @weak nav_entry => move |_, _| {
-            let conformant_url = &web_view.get_uri().unwrap().to_string().replacen("http://", "ipfs://", 1).replacen(".ipfs.localhost:8080", "", 1);
-            nav_entry.set_text(conformant_url);
-        }));
-    });
+    nav_entry.connect_activate(clone!(@weak web_view, @weak nav_entry => move |_| {
+        connect(nav_entry, web_view)
+    }));
+    go_button.connect_clicked(clone!(@weak web_view, @weak nav_entry => move |_| {
+        connect(nav_entry, web_view);
+    }));
+    web_view.connect_load_changed(clone!(@weak web_view, @weak nav_entry => move |_, _| {
+        on_load(nav_entry, web_view)
+    }));
 
     window.show_all();
     window.connect_delete_event(|_, _| {
