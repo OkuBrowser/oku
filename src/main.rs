56e68ee82e9017eb3c57b7be726c196ecdf7e16d
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2021-12-30 13:50:48 -0500

    work on tab audio indicator

--- src/main.rs
+++ src/main.rs
@@ -361,14 +361,14 @@ fn new_view(
             true => {
                 current_page.set_indicator_icon(Some(&gio::ThemedIcon::new("notification-audio-volume-high")));
                 current_page.set_indicator_activatable(true);
-                // current_page.connect_indicator_activated_notify(clone!(@weak web_view => move |_| {
-                //     web_view.set_is_muted(true);
-                // }));
+                current_page.connect_indicator_icon_notify(clone!(@weak web_view => move |_| {
+                    web_view.set_is_muted(!web_view.get_is_muted());
+                }));
             },
             false => {
                 if !web_view.is_muted()
                 {
-                    current_page.set_indicator_icon(Some(&gio::BytesIcon::new(&glib::Bytes::from(b""))));
+                    current_page.set_indicator_icon(None);
                     current_page.set_indicator_activatable(false);
                 }
             }
