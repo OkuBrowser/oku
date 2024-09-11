b2c2cd71737f30647fa7ed72c5be903303d44146
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2024-09-11 16:17:40 -0400

    Flatpak packaging

--- install_flatpak.sh
+++ install_flatpak.sh
@@ -2,4 +2,5 @@
 curl --output "flatpak-cargo-generator.py" "https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/master/cargo/flatpak-cargo-generator.py"
 python3 ./flatpak-cargo-generator.py ./Cargo.lock -o "./build-aux/cargo-sources.json"
 sudo cp -ar ./data/hicolor /usr/share/icons
-flatpak-builder --install repo build-aux/com.github.dirout.oku.json --force-clean --user --install-deps-from=flathub -y
+flatpak run org.flatpak.Builder --force-clean --user --install --install-deps-from=flathub --ccache --mirror-screenshots-url=https://dl.flathub.org/media/ --repo=repo builddir build-aux/com.github.dirout.oku.json
+flatpak build-bundle repo oku.flatpak com.github.dirout.oku --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
