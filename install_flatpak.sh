0af9be4bb520049b0b8dca6ea12034de7c3ce60d
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2024-11-11 21:22:30 -0500

    feat: Flatpak screenshots

--- install_flatpak.sh
+++ install_flatpak.sh
@@ -4,3 +4,4 @@ curl --output "flatpak-cargo-generator.py" "https://raw.githubusercontent.com/fl
 python3 ./flatpak-cargo-generator.py ./Cargo.lock -o "./build-aux/cargo-sources.json"
 flatpak run org.flatpak.Builder --force-clean --user --install --install-deps-from=flathub --ccache --mirror-screenshots-url=https://dl.flathub.org/media/ --repo=repo builddir build-aux/io.github.OkuBrowser.oku.json
 flatpak build-bundle repo oku.flatpak io.github.OkuBrowser.oku --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
+ostree commit --repo=repo --canonical-permissions --branch=screenshots/x86_64 builddir/files/share/app-info/media
