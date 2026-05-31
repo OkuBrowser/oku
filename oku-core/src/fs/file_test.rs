cf654de1be34cee5f398068644f423907a6bf996
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2026-05-30 21:42:15 -0700

    Update file_test.rs

--- oku-core/src/fs/file_test.rs
+++ oku-core/src/fs/file_test.rs
@@ -3,11 +3,9 @@ mod tests {
     use std::{path::PathBuf, str::FromStr};
 
     #[tokio::test]
-    async fn test_read_file() -> Result<(), Box<dyn std::error::Error>> {
+    async fn test_files() -> Result<(), Box<dyn std::error::Error>> {
         let file_contents = "Hello, World!";
-        println!("A");
         let file_path = PathBuf::from_str("/test.txt")?;
-        println!("B");
         let node = crate::fs::OkuFs::start(
             #[cfg(feature = "fuse")]
             None,
@@ -15,16 +13,26 @@ mod tests {
             false,
         )
         .await?;
-        println!("C");
         let replica_id = node.create_replica().await?;
-        println!("D");
-        node.create_or_modify_file(&replica_id, &file_path, file_contents)
+
+        // Test creation and reading
+        let file_hash = node
+            .create_or_modify_file(&replica_id, &file_path, file_contents)
             .await?;
-        println!("E");
         assert_eq!(
             node.read_file(&replica_id, &file_path).await?,
             file_contents
         );
+
+        // Test listing
+        let file_list = node.list_files(&replica_id, &None).await?;
+        assert_eq!(1, file_list.len());
+        assert_eq!(Some(file_hash), file_list.first().map(|x| x.content_hash()));
+
+        // Test entry retrieval
+        let file_entry = node.get_entry(&replica_id, &file_path).await?;
+        assert_eq!(Some(file_entry), file_list.first().cloned());
+
         Ok(())
     }
 }
